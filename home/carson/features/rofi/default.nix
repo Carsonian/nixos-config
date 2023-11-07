ea9b967421c29bfd0a4830ee173b9d51a50f1e1e
Carson Moore <carson2477@live.com>
2023-11-08 00:39:12 +0700

    added new rofi theme

--- home/carson/features/rofi/default.nix
+++ home/carson/features/rofi/default.nix
@@ -8,8 +8,10 @@
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
-    theme = ./gruvbox-dark-theme.rasi;
+    theme = ./style-theme.rasi;
     font = "JetBrains Mono Nerd Font";
     plugins = [ pkgs.rofi-power-menu pkgs.rofi-calc pkgs.rofi-mpd ];
+    extraConfig = {
+    };
   };
 }
